db18671641021a6486506cd418cc7fa15df53e7c
brightonchege <brightonspak@gmail.com>
2022-08-23 08:57:21 +0300

    complete

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let totalBatteries = batteryBatches.reduce((value1,value2)=>{
+    return value2 + value1
+})
 
-// Code your solution here
+
+
+
+
+
+
